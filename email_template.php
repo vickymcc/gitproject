c1b880ad0795fbf3b5ae4f315dd98989d8b2d46c
vickymcc <vicky@multichannelcreative.co.uk>
2019-01-24 14:34:40 +0530

    add folder & image

--- email_template.php
+++ email_template.php
@@ -3,29 +3,56 @@
         <title>
             Email template
         </title>
+        <style>
+            @font-face {
+                font-family: roboto_bold;
+                src: url(fonts/Roboto-Bold.ttf);
+            }
+            @font-face {
+                font-family: roboto_regular;
+                src: url(fonts/Roboto-Regular.ttf);
+            }
+            @font-face {
+                font-family: roboto_medium;
+                src: url(fonts/Roboto-Medium.ttf);
+            }
+            @font-face {
+                font-family: roboto_light;
+                src: url(fonts/Roboto-Light.ttf);
+            }
+            .light-font{font-family:roboto_light}
+            .bold_font{font-family:roboto_bold}
+            .font{font-size:30px;font-family: roboto_bold}
+            .regular_font{font-family:roboto_regular}
+            .medium_font{font-family: roboto_medium}
+            .padding-left{padding-left:10px;}
+            .footer-color{background-color: dimgrey;color:white;font-family: roboto_regular}
+        </style>
     </head>
     <body>
+        <table style="width:80%;padding-right:15px;padding-left:15px;margin-right:auto;margin-left:auto;padding-top: 50px;background-color: dimgrey;color:white;">
+            <tr style="" >
+                <td  style="" >
+                    logo image
+                </td>
+                <td align="right"  style="" >
+                    section three
+                </td>
+            </tr>
+        </table>
         <table style="width:80%;padding-right:15px;padding-left:15px;margin-right:auto;margin-left:auto;">
             <tbody>
                 <tr>
-                    <th  style="background-color: lightgoldenrodyellow;" >
-                        logo image
-                    </th>
-                    <th align="right"  style="background-color: lightgreen;" >
-                        section three
-                    </th>
-                </tr>
-                <tr>
                     <td>
-                        Information
-                        <div style="font-size:20px;" >
+                        <span class="regular_font" >Information</span>
+                        <div class="font">
                             The below text is a description... 
                         </div>
                     </td>
                     <td>
                     </td>
                 </tr>
-                <tr>
+                <tr class="medium_font" >
                     <td>
                         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus
                     </td>
@@ -39,12 +66,37 @@
             <tbody>
                 <tr>
                     <td align="center" >
-                        <h2>Welcome to template</h2>
-                        <div>The proportion of the introduction section...</div>
+                        <h2 class="bold_font">Welcome to template</h2>
+                        <div class="regular_font">The proportion of the introduction section...</div>
                         <button style="margin-top: 15px;padding: 10px 25px 10px 25px;background-color: lightblue;border: none;" >Click here</button>
-                        <p>
+                        <p class="regular_font">
                             imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imp
                         </p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <table style="width:80%;padding-right:15px;padding-left:15px;margin-right:auto;margin-left:auto;padding-top: 50px">
+            <tbody>
+                <tr class="light-font" >
+                    <td align="left" >
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centur
+                    </td>
+                    <td align="left" >
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centur
+                    </td>
+                    <td align="left" >
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centur
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <table style="width:80%;padding-right:15px;padding-left:15px;margin-right:auto;margin-left:auto;padding-top: 50px">
+            <tbody>
+                <tr class="footer-color" >
+                    <td align="center" >
+                        <p class="bold_font"><span  >Contact us</span><span class="padding-left" >About us</span><span class="padding-left" >Home</span></p>
+                        <span>Lorem Ipsum is simply dummy text of th</span>
                     </td>
                 </tr>
             </tbody>
