2059d8a89a8230a109f5322a8a4a9954af39902a
vickymcc <vicky@multichannelcreative.co.uk>
2019-01-24 11:59:40 +0530

    add html

---
+++ email_template.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+        <title>
+            Email template
+        </title>
+    </head>
+    <body>
+        <table style="width:80%;padding-right:15px;padding-left:15px;margin-right:auto;margin-left:auto;">
+            <tbody>
+                <tr>
+                    <th  style="background-color: lightgoldenrodyellow;" >
+                        logo image
+                    </th>
+                    <th align="right"  style="background-color: lightgreen;" >
+                        section three
+                    </th>
+                </tr>
+                <tr>
+                    <td>
+                        Information
+                        <div style="font-size:20px;" >
+                            The below text is a description... 
+                        </div>
+                    </td>
+                    <td>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus
+                    </td>
+                    <td>
+                        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,   
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <table style="width:80%;padding-right:15px;padding-left:15px;margin-right:auto;margin-left:auto;padding-top: 50px">
+            <tbody>
+                <tr>
+                    <td align="center" >
+                        <h2>Welcome to template</h2>
+                        <div>The proportion of the introduction section...</div>
+                        <button style="margin-top: 15px;padding: 10px 25px 10px 25px;background-color: lightblue;border: none;" >Click here</button>
+                        <p>
+                            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imp
+                        </p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
